9a73e42da96901b039dd4ec0c88d0952589a723f
Michael Sakevych <sakevich2001@gmail.com>
2022-12-03 19:59:47 -0800

    Fix default profile value in the simulator

--- sim/main.cc
+++ sim/main.cc
@@ -6,10 +6,10 @@
 
 int main(int argc, char** argv) {
   std::string sched = "sched.csv";
-  std::string trips = "trips.csv";
+  std::string prof = "profile.csv";
   if (argc > 1) sched = std::string(argv[1]);
-  if (argc > 2) trips = std::string(argv[2]);
+  if (argc > 2) prof = std::string(argv[2]);
   Schedule s(sched);
-  Evaluator e(trips);
+  Evaluator e(prof);
   std::cout << pretty_d(e.eval(s)) << std::endl;
 }
