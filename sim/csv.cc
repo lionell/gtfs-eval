9a9234c0f566f1ed3ab491a189dc089c9e3021a1
Michael Sakevych <sakevich2001@gmail.com>
2022-12-03 19:58:33 -0800

    Add example travel profile (random 10k trips)

--- sim/csv.cc
+++ sim/csv.cc
@@ -2,14 +2,13 @@
 
 std::vector<std::string> split(const std::string& s) {
   static const char DELIM = ',';
-
-	std::vector<std::string> res;
-	size_t start = 0;
-	size_t finish = -1;
-	while ((finish = s.find(DELIM, start)) != std::string::npos) {
-		res.push_back(s.substr(start, finish - start));
-		start = finish + 1;
-	}
-	res.push_back(s.substr(start));
-	return res;
+  std::vector<std::string> res;
+  size_t start = 0;
+  size_t finish = -1;
+  while ((finish = s.find(DELIM, start)) != std::string::npos) {
+    res.push_back(s.substr(start, finish - start));
+    start = finish + 1;
+  }
+  res.push_back(s.substr(start));
+  return res;
 }
